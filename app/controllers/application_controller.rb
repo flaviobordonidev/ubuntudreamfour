2620764181c1f819f3c1d33afb7c0d568f1d2a1d
Flavio Bordoni <flavio.bordoni.dev@gmail.com>
2024-02-18 16:03:20 +0100

    set locale via url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+    before_action :set_locale
+
+    #---
+    private
+    
+    #set language for internationalization
+    def set_locale
+      case params[:locale]
+      when "it", "en"
+        I18n.locale = params[:locale]
+      else
+        I18n.locale = I18n.default_locale
+      end
+    end  
 end
